f5aa4cd42326f78db5a98601b55a08db66bcea05
Marcos Vinicius Conceição Oliveira <vinioliveira2008y@gmail.com>
2024-10-08 17:08:55 -0300

    build: dependências externas

---
+++ API/TodoApi/Context/AgendaContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using Microsoft.Net.Http.Headers;
+
+
+namespace Context
+{
+    public class AgendaContext : DbContext
+    {
+       public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+       {
+
+       }
+
+       public DbSet<contato> Contatos( get; set; )
+       
+    }
+
+    internal class set
+    {
+    }
+
+    public class get
+    {
+    }
+}
--- API/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ API/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03311fed6eb6cbec2d985efdf3967794106b459d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d36077254cd0332bfd152863032a8423e262d17")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
